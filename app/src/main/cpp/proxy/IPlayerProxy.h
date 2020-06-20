f79f10efaef89d5cb7f933366000439edd531f9c
wy <ky6115@163.com>
2020-06-20 17:07:11 +0800

    proxy ok了

---
+++ app/src/main/cpp/proxy/IPlayerProxy.h
@@ -0,0 +1,37 @@
+//
+// Created by ky611 on 2020/6/20.
+//
+
+#ifndef XPLAY_IPLAYERPROXY_H
+#define XPLAY_IPLAYERPROXY_H
+
+
+#include <mutex>
+#include "../player/IPlayer.h"
+
+class IPlayerProxy : public IPlayer {
+
+public:
+    void init();
+
+    static IPlayerProxy *get() {
+        static IPlayerProxy proxy;
+        return &proxy;
+    }
+
+    bool open(const char *path) override;
+
+    bool initView(void *window) override;
+
+    void start() override;
+
+protected:
+
+protected:
+    IPlayerProxy() {};
+    IPlayer *player = nullptr;
+    std::mutex mutex;
+};
+
+
+#endif //XPLAY_IPLAYERPROXY_H
--- app/src/main/cpp/xplay.cpp
+++ app/src/main/cpp/xplay.cpp
@@ -5,7 +5,7 @@
 #include "player/IPlayer.h"
 #include "IPlayerBuilder.h"
 #include "FFPlayerBuilder.h"
-
+#include "proxy/IPlayerProxy.h"
 
 extern "C" {
 #include "libavcodec/avcodec.h"
@@ -14,24 +14,18 @@ extern "C" {
 
 ANativeWindow *window;
 
-static IPlayer *player = NULL;
-
 
 extern "C"
 JNIEXPORT jboolean JNICALL
 Java_com_joeys_xplay_Xplay_open(JNIEnv *env, jobject thiz, jstring _url) {
     using namespace std;
     const char *url = env->GetStringUTFChars(_url, 0);
-
-    player = FFPlayerBuilder::get()->buildPlayer();
-
-    player->open(url);
+    IPlayerProxy::get()->init();
+    IPlayerProxy::get()->open(url);
     if (window) {
-        player->initView(window);
+        IPlayerProxy::get()->initView(window);
     }
-
-    player->start();
-
+    IPlayerProxy::get()->start();
     return 0;
 }
 
@@ -40,8 +34,8 @@ JNIEXPORT void JNICALL
 Java_com_joeys_xplay_Xplay_initView(JNIEnv *env, jobject thiz, jobject holder) {
     window = ANativeWindow_fromSurface(env, holder);
     LOGD("ANativeWindow_fromSurface %d", window);
-    if (window && player) {
-        player->initView(window);
+    if (window) {
+        IPlayerProxy::get()->initView(window);
     }
 }
 
